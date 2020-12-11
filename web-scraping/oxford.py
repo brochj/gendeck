bd67e4823ca673f68e2c05e966eb06da6bac8e4b
brochj <brochj@gmail.com>
2020-12-11 19:19:49 -0300

    oxford web-scraping

--- web-scraping/oxford.py
+++ web-scraping/oxford.py
@@ -7,68 +7,118 @@ Created on Fri Dec 11 11:34:13 2020
 
 from bs4 import BeautifulSoup
 import requests
-import time
-
-WORDS = ['umbrella'] 
 
 
-URL=f"https://www.oxfordlearnersdictionaries.com/us/definition/english/{WORDS[0]}"
-
-# URL=f"https://www.google.com/search?q={WORDS[0]}"
-
-# Make a GET request to fetch the raw HTML content
-html_content = requests.get(URL,headers={"User-Agent":"Mozilla/5.0"}).text
-
-# Parse the html content
-soup = BeautifulSoup(html_content, 'html.parser')
-
-
-def get_definitions(_soup):
+class Oxford:
     
-    definitions_htmls = _soup.find_all("span", class_="def")
-    definitions = []
+    def __init__(self, word):
+        self.word = word
+        self.soup = None
+        self.get_html()
+
+        self.definitions = []
+        self.examples = []
+        self.ipa_nam = ''
+        self.ipa_br = ''
+        self.word_type = '' 
+        self.word_level = ''
+        
+        self.get_definitions()
+        self.get_examples()
+        self.get_ipa()
+        self.get_ipa('br')
+        self.get_word_type()
+        self.get_word_level()
+        # self.word_level = self.get_word_level()
     
-    for definition in definitions_htmls:
-        definitions.append(definition.text)
-    return definitions
+    def get_html(self):
+        URL=f"https://www.oxfordlearnersdictionaries.com/us/definition/english/{self.word}"
+        # Make a GET request to fetch the raw HTML content
+        html_content = requests.get(URL,headers={"User-Agent":"Mozilla/5.0"}).text
 
-
-def get_examples(_soup):
-    examples_html = _soup.find_all("ul", class_="examples", hclass="examples")
-    examples = []
+        # Parse the html content
+        self.soup = BeautifulSoup(html_content, 'html.parser')
+        
     
-    for example_ul in examples_html:
-        example_list = [ex.text for ex in example_ul.find_all('li')]
-        examples.append(example_list)
-    return examples
-      
-
-def get_ipa(_soup, phon="nam"):
-    ipa_nam = _soup.find("div", class_="phons_n_am").text
-    ipa_br = _soup.find("div", class_="phons_br").text
     
-    return ipa_nam if phon=="nam" else ipa_br
+    def get_definitions(self):
+        try:
+            definitions_html = self.soup.find("ol", class_="senses_multiple")
+            definitions_li = definitions_html.find_all("span", class_="def")
+           
+            for definition in definitions_li:
+                self.definitions.append(definition.text)
+        except:
+            print("Can't find definitions")
 
-def get_word_type(_soup):
-    return _soup.find("span", class_="pos").text
-
-
-def get_word_level(_soup):
-    level_html = _soup.find("div", class_="symbols")
-    link = level_html.contents[0].get('href')
-    level = link[-2:]
-    return level
-
-
-# definitions = get_definitions(soup)
-# examples = get_examples(soup)
-# nam = get_ipa(soup)
-# br = get_ipa(soup, 'br')
-# word_type = get_word_type(soup)
-
-word_level = get_word_level(soup)
+    def clear_extra_examples(self):
+        try:
+            self.soup.find("span", unbox="extra_examples").clear() #extra_examples_removed
+            
+        except:
+            print("Doesn't exist 'extra examples or Can't clear 'extra examples'")
 
 
+    def get_examples(self):
+        try:
+            self.clear_extra_examples()
+            definitions_html = self.soup.find("ol", class_="senses_multiple")
+            examples_html = definitions_html.find_all("ul", class_="examples", hclass="examples")
+            
+            for example_ul in examples_html:
+                example_list = [ex.text for ex in example_ul.find_all('li')]
+                self.examples.append(example_list)
+        except:
+            print("Can't find examples")
 
 
+    def get_extra_examples():
+        return
+          
+
+    def get_ipa(self, phon="nam"):
+        try:
+            self.ipa_nam = self.soup.find("div", class_="phons_n_am").text
+            self.ipa_br = self.soup.find("div", class_="phons_br").text
+
+        except:
+            print("Can't find ipa")
+
+    def get_word_type(self):
+        try:
+            self.word_type = self.soup.find("span", class_="pos").text    
+        except:
+            print("Can't find word type")
+
+
+    def get_word_level(self):
+        try:
+            level_html = self.soup.find("div", class_="symbols")
+            link = level_html.contents[0].get('href')
+            self.word_level = link[-2:]
+        except:
+            print("Can't find word level")
+
+    def get_idioms(self):
+        #TODO
+        idioms_html = self.soup.find("div", class_="idioms")
+        print(idioms_html)
+        print(len(idioms_html))
+        # examples_html = idioms_html.find_all("ol", class_="examples", hclass="examples")
+        
+        return idioms_html
+
+
+
+
+
+if __name__ == "__main__":
+    teste = Oxford('umbrella')
+
+    definitions = teste.definitions
+    examples = teste.examples
+    nam = teste.ipa_nam
+    br = teste.ipa_br
+    word_type = teste.word_type
+    word_level = teste.word_level
 

528a5ea1ec3cd8fd71c9320cd7e35dcd16590051
brochj <brochj@gmail.com>
2020-12-16 15:32:45 -0300

    Add google images button

--- main.py
+++ main.py
@@ -73,26 +73,27 @@ my_model = genanki.Model(
         {"name": "Example"},
         {"name": "Definition"},
         # # {'name': 'id'},
-        {"name": "IPA Nam"},
+        {"name": "IPA NAm"},
         {"name": "IPA Br"},
         {"name": "English Level"},
         {"name": "Word Type"},
-        # {"name": "Word Group"},
-        # # {'name': 'Image'},
+        {"name": "Word Group"},   
         # # {'name': 'Audio_word'},
-        # # {'name': 'Google images'},
+        {'name': 'Google_images'},
         {"name": "Dict_link"},
-        # {'name': 'Tags'},
+        {"name": "Longman_link"},
+        {'name': 'Image'},
+        {'name': 'Tags'},
     ],
     templates=[
         {
             "name": "Card",
-            "qfmt": "{{cloze::Example}}<br/><br/>Def: <b>{{Definition}}</b>",
-            "afmt": '{{cloze::Example}}<br/><br/>Def: <b>{{Definition}}</b><hr id="answer"><word>{{Word}}</word> <sub><i>{{Word Type}}</i></sub><br/><b>{{IPA Nam}}</b><br/><br/>Oxford Level: <level>{{English Level}}</level><br/><a href="{{Dict_link}}" class="oxfordButton">Check up on Oxford Dictionary</a>',
+            "qfmt": "{{cloze::Example}}<br/><br/><b>Def:</b> {{Definition}}<br/><br/>{{Image}}",
+            "afmt": '{{cloze::Example}}<br/><br/><b>Def:</b> {{Definition}}<br/><br/>{{Image}}<hr id="answer"><word>{{Word}}</word> <sub><i>{{Word Type}}</i></sub><br/><b>{{IPA NAm}}</b><br/><br/>Oxford Level: <level>{{English Level}}</level><br/>Longman Group: <wordgroup>{{Word Group}}</wordgroup><br/><br/><a href="{{Dict_link}}" class="oxfordButton">Oxford Dictionary</a><a href="{{Longman_link}}" class="longmanButton">Longman Dictionary</a><a href="{{Google_images}}" class="googleButton">Google Images</a>',
         },
     ],
-    css=".card{font-family:arial;font-size:20px;text-align:center;color:#000;background-color:#fff}.cloze{font-weight:700;color:#00f}word{font-weight:700;color:#00f}level{font-weight:700;color:#1e90ff}.oxfordButton{background:linear-gradient(to bottom,#3d94f6 5%,#1e62d0 100%);background-color:#3d94f6;border-radius:6px;border:1px solid #094793;display:inline-block;cursor:pointer;color:#fff;font-family:Arial;font-size:15px;font-weight:700;padding:6px 24px;text-decoration:none}.oxfordButton:hover{background:linear-gradient(to bottom,#1e62d0 5%,#3d94f6 100%);background-color:#1e62d0}.oxfordButton:active{position:relative;top:1px}",
-    # model_type= 0 #CLOZE=1
+    css=".card{font-family: arial;font-size: 20px;text-align: center;color: #000;background-color: #fff}.cloze{font-weight: 700;color: #00f}word{font-weight: 700;color: #00f}level{font-weight: 700;color: #1e90ff}.oxfordButton{background: linear-gradient(to bottom, #3d94f6 5%, #1e62d0 100%);background-color: #3d94f6;border-radius: 6px;border: 1px solid #094793;display: inline-block;cursor: pointer;color: #fff;font-family: Arial;font-size: 15px;font-weight: 700;padding: 6px 24px;text-decoration: none;margin:10px}.oxfordButton:hover{background: linear-gradient(to bottom, #1e62d0 5%, #3d94f6 100%);background-color: #1e62d0}.oxfordButton:active{position: relative;top: 1px}.longmanButton{background:linear-gradient(to bottom, #3d94f6 5%, #314089 100%);background-color:#3d94f6;border-radius:6px;border: 1px solid #094793;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:15px;font-weight:bold;padding:6px 24px;text-decoration:none;margin:10px}.longmanButton:hover{background:linear-gradient(to bottom, #314089 5%, #3d94f6 100%);background-color:#314089}.longmanButton:active{position:relative;top:1px}.googleButton{background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);background-color:#f9f9f9;border-radius:6px;border:1px solid #dcdcdc;display:inline-block;cursor:pointer;color:#666666;font-family:Arial;font-size:15px;font-weight:bold;padding:6px 24px;text-decoration:none;margin:10px}.googleButton:hover{background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);background-color:#e9e9e9}.googleButton:active{position:relative;top:1px}img{max-width:400px;height:auto;border-radius: 20px}wordgroup{font-weight: 700;color: #F1431E}",
+
 )
 
 
@@ -106,7 +107,7 @@ my_deck = genanki.Deck(2106080373, "Longman")
 for word_def in words_dict:
     for word in words_list:
         if word_def["word"] == word["word"]:
-
+            print(word)
             my_note = genanki.Note(
                 model=my_model,
                 fields=[
@@ -119,31 +120,23 @@ for word_def in words_dict:
                     word_def['ipa_br'],
                     word_def['word_level'].upper(),
                     word_def['word_type'],
-                    # word['group'],
-                    f'https://www.oxfordlearnersdictionaries.com/us/definition/english/{word["word"]}'
+                    ' '.join(word['group']),
+                    f'http://www.google.com/search?q={word["word"]}&tbm=isch',
+                    f'https://www.oxfordlearnersdictionaries.com/us/definition/english/{word["word"]}',
+                    f'https://www.ldoceonline.com/dictionary/{word["word"]}',
+                    '<img src="Image_2.jpg">',
+                    ' '.join(word["group"] + list(word_def['word_level'].upper().split()) + word_def['word_type'].split()),
                     
                     
                 ],
-                tags=word["group"] + word_def['word_type'].split(),
+                tags=word["group"] + list(word_def['word_level'].upper().split()) + word_def['word_type'].split(),
             )
-            print([
-                    word["word"],
-                    word_def["example"].replace(
-                        word["word"], "{{c1::" + word["word"] + "}}"
-                    ),
-                    word_def["definition"],
-                    word_def['ipa_nam'],
-                    word_def['ipa_br'],
-                    word_def['word_level'].upper(),
-                    word_def['word_type'],
-                    str(word['group']).replace('[','').replace(']','').replace(',','').replace("'",''),
-                    f'https://www.oxfordlearnersdictionaries.com/us/definition/english/{word["word"]}'
-                    
-                    
-                ]
-                
-                )
+            
 
             my_deck.add_note(my_note)
-
-            genanki.Package(my_deck).write_to_file("output.apkg")
+            
+            my_package = genanki.Package(my_deck)
+            
+            my_package.media_files = ['images/dog/Image_2.jpg']
+            
+            my_package.write_to_file("output.apkg")
